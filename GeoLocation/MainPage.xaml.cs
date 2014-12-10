8888d4720a67bebc5c902baf0d8e68d7356fb17d
d.shimanov <d.a.shimanov@gmail.com>
2014-12-10 21:51:50 +0600

    Geolocation
    Geolocation for WP 8.1 xample

---
+++ GeoLocation/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Geolocation;
+
+namespace GeoLocation
+{
+    public sealed partial class MainPage : Page
+    {
+        Geolocator geo = null;
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (geo == null)
+            {
+                geo = new Geolocator();
+            }
+
+            Geoposition pos = await geo.GetGeopositionAsync();
+            latitude.Text = "Широта:" + pos.Coordinate.Point.Position.Latitude.ToString();
+            lohgitue.Text = "Долгота:" + pos.Coordinate.Point.Position.Longitude.ToString();
+            accuracy.Text = "Точность" + pos.Coordinate.Accuracy.ToString();
+        }
+    }
+}
