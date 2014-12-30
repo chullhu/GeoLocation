69bf240e1f00abae3a943f5e82b6ee75749e49a0
Chtyllhu <d.a.shimanov@gmail.com>
2014-12-30 20:56:14 +0600

    Ready!

--- GeoLocation/MainPage.xaml.cs
+++ GeoLocation/MainPage.xaml.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-//using System.Collections.Generic;
-//using System.IO;
-//using System.Linq;
-//using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -41,7 +37,7 @@ namespace GeoLocation
             Geoposition pos = await geo.GetGeopositionAsync();
             latitude.Text = "Широта:" + pos.Coordinate.Point.Position.Latitude.ToString();
             lohgitue.Text = "Долгота:" + pos.Coordinate.Point.Position.Longitude.ToString();
-            accuracy.Text = "Точность" + pos.Coordinate.Accuracy.ToString();
+            accuracy.Text = "Точность:" + pos.Coordinate.Accuracy.ToString();
         }
     }
 }
